d0e9cac046c195b1ca06be6f4b887528aa66dc18
Aleksey <mr.ryabov.aa@yandex.ru>
2025-03-30 15:09:17 +0300

    Create project in intellij idea

---
+++ src/main/java/ru/ryaboman/projects/ntsystem/NtSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.ryaboman.projects.ntsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ryaboman/projects/ntsystem/NtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ryaboman.projects.ntsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
