fbe266fff3f23c0fea8c920ecd326db9293c1c79
Aleksey <mr.ryabov.aa@yandex.ru>
2025-04-18 18:11:42 +0300

    Добавляет валидацию для пользователя

--- backend/src/main/java/ru/ryaboman/projects/ntsystem/backend/entity/User.java
+++ backend/src/main/java/ru/ryaboman/projects/ntsystem/backend/entity/User.java
@@ -1,6 +1,8 @@
 package ru.ryaboman.projects.ntsystem.backend.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,8 @@ public class User {
     private Long id;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Username не должно быть пустым")
+    @Min(value = 2, message = "Минимальная длинна username равна 2")
     private String username;
 
     @Column(nullable = false)
