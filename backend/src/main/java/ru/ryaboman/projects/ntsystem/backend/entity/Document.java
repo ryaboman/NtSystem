ba1f71beb03c4f0f764a6271b70a21610c4dbf67
Aleksey <mr.ryabov.aa@yandex.ru>
2025-04-22 22:23:49 +0300

    Добавляет changelog создания таблицы документов в БД

--- backend/src/main/java/ru/ryaboman/projects/ntsystem/backend/entity/Document.java
+++ backend/src/main/java/ru/ryaboman/projects/ntsystem/backend/entity/Document.java
@@ -1,6 +1,14 @@
 package ru.ryaboman.projects.ntsystem.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "documents")
+@Data
 public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String title;
     private String description;
