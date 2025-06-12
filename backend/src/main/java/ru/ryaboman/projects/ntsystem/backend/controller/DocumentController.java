576695ef233bee3d13fc6d9d22826997af7e988e
Aleksey <mr.ryabov.aa@yandex.ru>
2025-06-12 20:14:30 +0300

    Добавляет endpoint поиска документа по id

--- backend/src/main/java/ru/ryaboman/projects/ntsystem/backend/controller/DocumentController.java
+++ backend/src/main/java/ru/ryaboman/projects/ntsystem/backend/controller/DocumentController.java
@@ -1,20 +1,33 @@
 package ru.ryaboman.projects.ntsystem.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.ryaboman.projects.ntsystem.backend.entity.Device;
 import ru.ryaboman.projects.ntsystem.backend.entity.Document;
+import ru.ryaboman.projects.ntsystem.backend.service.DocumentService;
+import ru.ryaboman.projects.ntsystem.backend.util.MinioUtil;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/documents")
 public class DocumentController {
-    @GetMapping("/documents")
+
+    private final DocumentService documentService;
+    private final MinioUtil minioUtil;
+
+    @GetMapping
     public String getDrawingPage() {
         return "drawing";
     }
 
-    @PostMapping("documents")
+    @GetMapping("/{id}")
+    public Document findByIdDocument(@PathVariable Long id) {
+        //minioUtil.createBucketName("files");
+        return documentService.findById(id);
+    }
+
+    @PostMapping
     public void addDocument(@RequestParam("document") Document document) {
 
     }
